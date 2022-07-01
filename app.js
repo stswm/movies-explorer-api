eaf91ae42545cf979ae51afd61bd0bfead85c6de
Oleg Azadov <oazadov@gmail.com>
2022-07-01 17:38:15 +0300

    helmet limiter

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const helmet = require('helmet');
+const limiter = require('./middlewares/limiter');
 const { errorHeandler } = require('./controllers/main');
 
 const { requestLogger, errorLogger } = require('./middlewares/logger');
-
 
 const routes = require('./routes');
 
@@ -13,6 +14,8 @@ const { PORT = 3000 } = process.env;
 
 mongoose.connect('mongodb://localhost:27017/moviedb');
 
+app.use(limiter);
+app.use(helmet());
 app.use(express.json());
 app.use(requestLogger);
 
