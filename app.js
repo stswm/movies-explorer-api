13a9d9e80b10ba66d0caf3a0d15c7b7af8d540a6
Oleg Azadov <oazadov@gmail.com>
2022-07-18 17:00:43 +0300

    fix try

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
 const helmet = require('helmet');
-const cors = require('./middlewares/cors');
+const cors = require('cors');
 const limiter = require('./middlewares/limiter');
 const { errorHeandler } = require('./controllers/main');
 const { ENV_PORT, DB_URL } = require('./utils/config');
@@ -22,8 +22,8 @@ app.use(limiter);
 app.use(helmet());
 app.use(express.json());
 
-app.use('/', routes);
 app.use(cors());
+app.use('/', routes);
 
 app.use(errorLogger);
 app.use(errors());
